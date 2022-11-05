072c84cbc823f14c1e5cdaabdee472825622c910
Arif Safiullah <prantor19@gmail.com>
2022-11-05 11:48:31 +0000

    understood boxed future lifetime issue

--- misc/src/main.rs
+++ misc/src/main.rs
@@ -1,41 +1,35 @@
-use std::pin::Pin;
-
-use futures::{future::join_all, Future, FutureExt};
+use futures::{future::BoxFuture, FutureExt};
 
 enum RecArg<'a> {
     Vec(Vec<&'a str>),
     String(&'a str),
 }
 
-fn rec_append<'a>(
-    x: RecArg<'a>,
-    mut init: Vec<&'a str>,
-) -> Pin<Box<dyn Future<Output = Vec<&'a str>>>> {
+fn rec_append<'a>(x: RecArg<'a>, mut init: Vec<&'a str>) -> BoxFuture<'a, Vec<&'a str>> {
     match x {
         RecArg::Vec(mut xs) => match xs.len() {
-            0 => async { init }.boxed_local(),
-            1 => rec_append(RecArg::String(xs.pop().unwrap()), init).boxed_local(),
+            0 => async { init }.boxed(),
+            1 => rec_append(RecArg::String(xs.pop().unwrap()), init).boxed(),
             _ => {
                 let x = xs.pop().unwrap();
                 rec_append(RecArg::String(x), init)
                     .then(|x_init| rec_append(RecArg::Vec(xs), x_init))
-                    .boxed_local()
+                    .boxed()
             }
         },
         RecArg::String(x) => {
             let test = async move {
-                init.extend(vec![x]);
+                init.extend(vec![":", x]);
                 init
             };
-            test.boxed_local()
+            test.boxed()
         }
     }
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let chars = vec!["a", "b", "c", "d", "e", "f"];
     let init = vec!["init"];
-    let test = rec_append(RecArg::Vec(chars), init).await;
-    dbg!(test);
+    let test = rec_append(RecArg::Vec(chars), init);
+    dbg!(futures::executor::block_on(test));
 }
